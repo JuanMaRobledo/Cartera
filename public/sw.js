81ebe4187f4118e3e80f124bffc884ae72f06225
JuanMaRobledo <juan0804@gmail.com>
2026-09-19 16:30:39 -0500

    Usar logo optimizado para carga móvil

--- public/sw.js
+++ public/sw.js
@@ -6,7 +6,7 @@ const SHELL_ASSETS = [
   "/icons/icon-512.png",
   "/icons/icon-maskable-512.png",
   "/icons/apple-touch-icon.png",
-  "/jmr-roble-foso-aprobado.png?v=1",
+  "/jmr-roble-foso-mobile.png?v=1",
 ];
 
 self.addEventListener("install", (event) => {
